a197b6ec57586554ebb1b01a4990bbc355fada05
BenPrzychodzki <34103010+BenPrzychodzki@users.noreply.github.com>
2022-03-19 18:01:48 +0100

    feat: szablon projektu

---
+++ core/app_config.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class AppConfig():
+    """A class containing all necessary variables for a game to run properly."""
+    FPS: int = 60
+    SCREEN_WIDTH: int = 800
+    SCREEN_HEIGHT: int = 800
+    PLAYER_WIDTH: int = 20
+    PLAYER_HEIGHT: int = 60
