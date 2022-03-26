7fd7b04b14f9dc50b59bb63d5dbc7e0d55de6cbd
BenPrzychodzki <34103010+BenPrzychodzki@users.noreply.github.com>
2022-03-26 11:36:54 +0100

    feat: stworzyc generacje platform

--- core/app_assets.py
+++ core/app_assets.py
@@ -3,3 +3,6 @@ import pygame
 class AppAssets:
     """Import all needed assets here."""
     player = pygame.image.load("assets/player.png")
+    left_platform_edge = pygame.image.load("assets/left_platform_edge.png")
+    middle_platform_part = pygame.image.load("assets/middle_platform_part.png")
+    right_platform_edge = pygame.image.load("assets/right_platform_edge.png")
--- pygame_template.py
+++ pygame_template.py
@@ -4,6 +4,7 @@ from core.app_assets import AppAssets
 
 from core.app_colors import AppColors
 from core.app_config import AppConfig
+from core.app_values_validator import validate_values
 
 
 # Initialize basic pygame stuff
@@ -12,9 +13,33 @@ WIN = pygame.display.set_mode((AppConfig.SCREEN_WIDTH, AppConfig.SCREEN_HEIGHT))
 CLOCK = pygame.time.Clock()
 
 
-class Player():
+class Platform:
+    def __init__(self, index):
+        self.index = index
+        
+        # Scale platform assets to values provided by an AppConfig file
+        self.image_left = pygame.transform.scale(AppAssets.left_platform_edge, (AppConfig.PLATFORM_PART_WIDTH, AppConfig.PLATFORM_HEIGHT))
+        self.image_middle = pygame.transform.scale(AppAssets.middle_platform_part, (AppConfig.PLATFORM_PART_WIDTH, AppConfig.PLATFORM_HEIGHT))
+        self.image_right = pygame.transform.scale(AppAssets.right_platform_edge, (AppConfig.PLATFORM_PART_WIDTH, AppConfig.PLATFORM_HEIGHT))
+        
+        self.length = random.randrange(AppConfig.MIN_PLATFORM_WIDTH, AppConfig.MAX_PLATFORM_WIDTH, AppConfig.PLATFORM_PART_WIDTH)
+        self.middle_part_quantity = (self.length - 2 * AppConfig.PLATFORM_PART_WIDTH) // AppConfig.PLATFORM_PART_WIDTH
+        self.pos = self.get_initial_pos()
+        
+    def get_initial_pos(self):
+        _pos_y = (AppConfig.SCREEN_HEIGHT - AppConfig.PLATFORM_HEIGHT) - self.index * AppConfig.DISTANCE_BETWEEN_PLATFORMS
+        return [random.randint(0, AppConfig.SCREEN_WIDTH - self.length), _pos_y]
+    
+    def draw_platform(self):
+        WIN.blit(self.image_left, (self.pos[0], self.pos[1]))
+        # Counts the position where the next middle part should be placed
+        for index in range(1, self.middle_part_quantity + 1):
+            WIN.blit(self.image_middle, (self.pos[0] + index * AppConfig.PLATFORM_PART_WIDTH, self.pos[1]))
+        WIN.blit(self.image_right, (self.pos[0] + self.length - AppConfig.PLATFORM_PART_WIDTH, self.pos[1]))
+            
+
+class Player:
     def __init__(self, starting_x, starting_y):
-        super().__init__()
         self.start_pos = [starting_x, starting_y]
         self.width = AppConfig.PLAYER_WIDTH
         self.height = AppConfig.PLAYER_HEIGHT
@@ -28,6 +53,7 @@ class GameView:
     def __init__(self):
         self.is_running = True
         self.player = Player(starting_x=500, starting_y=500)
+        self.platforms = self.create_platforms()
         self.game_loop()
         
     def game_loop(self):
@@ -40,10 +66,11 @@ class GameView:
             
     def redraw_window(self):
         WIN.fill(AppColors.WHITE)
+        for platform in self.platforms:
+            platform.draw_platform()
         WIN.blit(self.player.image, (self.player.rect.x, self.player.rect.y))
         pygame.display.update()
-        print("hello")
-        
+                
     def handle_events(self):
         for event in pygame.event.get():
             print(event)
@@ -55,8 +82,15 @@ class GameView:
         if pressed_keys[pygame.K_LEFT]:
             pass
         
+    def create_platforms(self):
+        _platforms = []
+        for platform_index in range(15):
+            _platforms.append(Platform(platform_index))
+        return _platforms
+        
 
 def main():
+    validate_values()
     GameView()
 
         
