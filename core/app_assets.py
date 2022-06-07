2753d58cf7cab8a166089fe80de98d427f7b92de
BenPrzychodzki <34103010+BenPrzychodzki@users.noreply.github.com>
2022-06-07 23:47:28 +0200

    add camera acceleration and score

--- core/app_assets.py
+++ core/app_assets.py
@@ -2,33 +2,33 @@ import pygame
 
 class AppAssets: #"D:\Python_programy\Icy_Tower\Icy-Tower-in-Python\assets"
     """Import all needed assets here."""
-    player = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/player1.png")     # zostawcie mi te sciezki bo nie chce mi sie co wersje je dodawac J
-    player_left = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/player1_left.png")
-    player_right = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/player1_right.png")
-    left_platform_edge = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/left_platform_edge.png")
-    middle_platform_part = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/middle_platform_part.png")
-    right_platform_edge = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/right_platform_edge.png")
-    start_platform = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/start_platform.png")
-    background = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/background.jpg")
-    frame = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/frame_right.png")
-    frame_1 = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/frame_left.png")
+    # player = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/player1.png")     # zostawcie mi te sciezki bo nie chce mi sie co wersje je dodawac J
+    # player_left = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/player1_left.png")
+    # player_right = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/player1_right.png")
+    # left_platform_edge = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/left_platform_edge.png")
+    # middle_platform_part = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/middle_platform_part.png")
+    # right_platform_edge = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/right_platform_edge.png")
+    # start_platform = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/start_platform.png")
+    # background = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/background.jpg")
+    # frame = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/frame_right.png")
+    # frame_1 = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/frame_left.png")
 
 
 
-    platform_sign = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/platform_sign.png")  # jacek dodaj sobie to
-    main_menu_pic = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/menu.png")
-    main_start_but = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/menu_start_game.png")
-    main_help_but = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/menu_help.png")
-    main_quit_but = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/menu_quit.png")
+    # platform_sign = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/platform_sign.png")  # jacek dodaj sobie to
+    # main_menu_pic = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/menu.png")
+    # main_start_but = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/menu_start_game.png")
+    # main_help_but = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/menu_help.png")
+    # main_quit_but = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/menu_quit.png")
 
-    game_is_over_pic = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/game_is_over_pic.png")
-    game_is_over_backtomain_pic = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/game_is_over_comeback_pic.png")
-    game_is_over_tryagain_pic = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/game_is_over_try_again_pic.png")
-
-
+    # game_is_over_pic = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/game_is_over_pic.png")
+    # game_is_over_backtomain_pic = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/game_is_over_comeback_pic.png")
+    # game_is_over_tryagain_pic = pygame.image.load("D:/Python_programy/Icy_Tower/Icy-Tower-in-Python/assets/game_is_over_try_again_pic.png")
 
 
-    '''player = pygame.image.load("assets/player1.png")
+
+
+    player = pygame.image.load("assets/player1.png")
     player_left = pygame.image.load("assets/player1_left.png")
     player_right = pygame.image.load("assets/player1_right.png")
     left_platform_edge = pygame.image.load("assets/left_platform_edge.png")
@@ -41,11 +41,12 @@ class AppAssets: #"D:\Python_programy\Icy_Tower\Icy-Tower-in-Python\assets"
     frame = pygame.image.load("assets/frame_right.png")
     frame_1 = pygame.image.load("assets/frame_left.png")
     platform_sign = pygame.image.load("assets/platform_sign.png")  # jacek dodaj sobie to
-    main_menu_pic = pygame.image.load("assets/menu.png")
-    main_start_but = pygame.image.load("assets/menu_start_game.png")
-    main_help_but = pygame.image.load("assets/menu_help.png")
-    main_quit_but = pygame.image.load("assets/menu_quit.png")
+    main_menu_image = pygame.image.load("assets/menu.png")
+    main_start_button = pygame.image.load("assets/menu_start_game.png")
+    main_help_button = pygame.image.load("assets/menu_help.png")
+    main_quit_button = pygame.image.load("assets/menu_quit.png")
 
     game_is_over_pic = pygame.image.load("assets/game_is_over_pic.png")
     game_is_over_backtomain_pic = pygame.image.load("assets/game_is_over_comeback_pic.png")
-    game_is_over_tryagain_pic = pygame.image.load("assets/game_is_over_try_again_pic.png")'''
+    game_is_over_tryagain_pic = pygame.image.load("assets/game_is_over_try_again_pic.png")
+    score_bar = pygame.image.load("assets/score_bar.png")
--- pygame_template.py
+++ pygame_template.py
@@ -7,11 +7,19 @@ from core.app_config import AppConfig
 from core.app_values_validator import validate_values
 pygame.font.init()
 font = pygame.font.Font('freesansbold.ttf', AppConfig.PLATFORM_SIGN_HEIGHT)
+score_font = pygame.font.SysFont('comicsans', 20, True)
 
 # Initialize basic pygame stuff
 pygame.font.init()
 WIN = pygame.display.set_mode((AppConfig.SCREEN_WIDTH, AppConfig.SCREEN_HEIGHT))
 CLOCK = pygame.time.Clock()
+
+PLATFORMS_SIZE_DICT = {
+        # PLATFORM_INDEX: (MIN_PLATFORM_WIDTH, MAX_PLATFORM_WIDTH)
+        0: (AppConfig.PLATFORM_PART_WIDTH * 10, AppConfig.PLATFORM_PART_WIDTH * 15),
+        1: (AppConfig.PLATFORM_PART_WIDTH * 6, AppConfig.PLATFORM_PART_WIDTH * 10),
+        2: (AppConfig.PLATFORM_PART_WIDTH * 3, AppConfig.PLATFORM_PART_WIDTH * 5),
+    }
 
 
 class Platform:
@@ -61,7 +69,7 @@ class Platform:
             _platform_sign_pos_x = self.pos[0] + (self.length - AppConfig.PLATFORM_SIGN_WIDTH) // 2
             _platform_sign_pos_y = self.pos[1] + (AppConfig.PLATFORM_HEIGHT - AppConfig.PLATFORM_SIGN_HEIGHT) / 2
 
-            platform_sign_text = font.render(str(self.index), True, AppColors.BLACK)
+            platform_sign_text = font.render(str(self.index), True, AppColors.WHITE)
             _platform_sign_text_pos_x = _platform_sign_pos_x + (
                         AppConfig.PLATFORM_SIGN_WIDTH - platform_sign_text.get_width()) // 2
 
@@ -72,7 +80,7 @@ class Platform:
         if self.index % AppConfig.DISTANCE_BETWEEN_LONG_PLATFORMS == 0:
             return AppConfig.SCREEN_WIDTH - 2 * AppConfig.FRAME_WIDTH
         else:
-            return random.randrange(AppConfig.MIN_PLATFORM_WIDTH, AppConfig.MAX_PLATFORM_WIDTH,
+            return random.randrange(PLATFORMS_SIZE_DICT[self.index // 100][0], PLATFORMS_SIZE_DICT[self.index // 100][1],
                                     AppConfig.PLATFORM_PART_WIDTH)
 
 
@@ -119,11 +127,15 @@ class GameView:
         self.background_speeed = 0
         self.height = -800
         self.game_is_over_pic = AppAssets.game_is_over_pic
+        self.score = 0
+        self.best_score = 0
+        self.timer = 0
+        self.camera_speed_factor = 1
+        
 
-        self.main_menu_pic = AppAssets.main_menu_pic
-        self.main_start_but = AppAssets.main_start_but
-       # self.main_help_but = AppAssets.main_help_but
-        self.main_quit_but = AppAssets.main_quit_but
+        self.main_menu_image = AppAssets.main_menu_image
+        self.main_start_button = AppAssets.main_start_button
+        self.main_quit_button = AppAssets.main_quit_button
 
 
         self.gameover_pic = AppAssets.game_is_over_pic
@@ -136,19 +148,18 @@ class GameView:
     def game_loop(self):
 
         while self.is_running:
-            print(self.player.moving_direction)
-
             if self.game_menu:
                 self.main_menu()
 
             elif self.game_is_running:
                 previous_y = self.player.rect.y
                 CLOCK.tick(AppConfig.FPS)
+                self.handle_timer()
                 self.redraw_window()
                 self.gravity()
                 self.handle_events()
                 self.handle_pressed_keys()
-                # self.Test_Mode()
+                # self.test_mode()
                 self.Camera_movement()
 
                 if self.player.collision == False:
@@ -159,6 +170,13 @@ class GameView:
 
             elif self.player.game_is_over:
                 self.gameower_menu()
+
+
+    def handle_timer(self):
+        self.timer += 1
+        if self.timer > 300:
+            self.timer = 0
+            self.camera_speed_factor += 0.1
 
 
     def redraw_window(self):
@@ -181,8 +199,27 @@ class GameView:
 
 
         WIN.blit(self.choose_player_image(), (self.player.rect.x, self.player.rect.y))
+        self.draw_score()
+        
+    
         pygame.display.update()
 
+    def draw_score(self):
+        WIN.blit(AppAssets.score_bar, (AppConfig.SCREEN_WIDTH * 0.01, AppConfig.SCREEN_HEIGHT * 0.01))
+
+        self.calculate_score()
+
+        font = pygame.font.SysFont('comicsans', 20, True)
+        score_text = font.render('Score: ' + str(self.score), True, AppColors.WHITE)
+        WIN.blit(score_text, (AppConfig.SCREEN_WIDTH * 0.05, AppConfig.SCREEN_HEIGHT * 0.025))
+
+    def calculate_score(self):
+        platform_passed = 0
+        for platform in self.platforms:
+            if platform.pos[1] > self.player.rect.y:
+                platform_passed += 1
+        self.score = platform_passed * 10
+            
 
     def choose_player_image(self):
         if not self.player.stays:
@@ -336,13 +373,13 @@ class GameView:
         if self.player.rect.y < AppConfig.CAMERA_START_GAME:
             self.game_over_time()
             for x in range(AppConfig.PLATFORMS_TO_GENERATE):
-                self.platforms[x].pos[1] += camera_speed
+                self.platforms[x].pos[1] += camera_speed * self.camera_speed_factor
             self.player.rect.y += player_y_dicrease
             AppConfig.CAMERA_START_GAME = inf
             self.background_speeed += camera_speed
 
 
-    def Test_Mode(self):
+    def test_mode(self):
         if pygame.key.get_pressed()[pygame.K_c]:
             self.background_speeed += AppConfig.CHEAT_POWER
             for i in range(AppConfig.PLATFORMS_TO_GENERATE):
@@ -374,34 +411,33 @@ class GameView:
 
     def main_menu(self):
 
-        picture = self.main_menu_pics()
+        picture = self.main_menu_images()
 
         for event in pygame.event.get():
 
             if event.type == pygame.MOUSEBUTTONUP:
-                if picture == self.main_start_but:
+                if picture == self.main_start_button:
                     self.game_is_running = True
                     self.game_menu = False
-
-                elif picture == self.main_help_but:
-                    pass
-                elif picture == self.main_quit_but:
+                elif picture == self.main_quit_button:
                     self.is_running = False
                     self.game_menu = False
+                else:
+                    pass
 
         WIN.fill(AppColors.BLACK)
-        WIN.blit(picture, ((AppConfig.SCREEN_WIDTH - self.main_menu_pic.get_width()) // 2, 0))
+        WIN.blit(picture, ((AppConfig.SCREEN_WIDTH - self.main_menu_image.get_width()) // 2, 0))
         pygame.display.update()
 
 
-    def main_menu_pics(self):
+    def main_menu_images(self):
 
         # staRtgamebutton
 
 
         if pygame.mouse.get_pos()[0] >= 310 and pygame.mouse.get_pos()[0] <= 544 and pygame.mouse.get_pos()[
             1] >= 352 and pygame.mouse.get_pos()[1] <= 410:
-            picture = self.main_start_but
+            picture = self.main_start_button
 
 
 
@@ -409,10 +445,10 @@ class GameView:
 
         elif pygame.mouse.get_pos()[0] >= 308 and pygame.mouse.get_pos()[0] <= 540 and pygame.mouse.get_pos()[
             1] >= 550 and pygame.mouse.get_pos()[1] <= 586:
-            picture = self.main_quit_but
+            picture = self.main_quit_button
 
         else:
-            picture = self.main_menu_pic
+            picture = self.main_menu_image
 
         return picture
 
@@ -442,6 +478,13 @@ class GameView:
 
         WIN.blit(picture, (AppConfig.SCREEN_WIDTH // 2 - self.game_is_over_pic.get_width() // 2,
                            AppConfig.SCREEN_HEIGHT // 2 - self.game_is_over_pic.get_height() // 2))
+                           
+        # show score on game over screen
+        score_text = score_font.render(str(self.score), True, AppColors.WHITE)
+        WIN.blit(score_text, (AppConfig.SCREEN_WIDTH * 0.5, AppConfig.SCREEN_HEIGHT * 0.49))
+        self.check_if_best_score()
+        best_score_text = score_font.render(str(self.best_score), True, AppColors.WHITE)
+        WIN.blit(best_score_text, (AppConfig.SCREEN_WIDTH * 0.55, AppConfig.SCREEN_HEIGHT * 0.52))
         pygame.display.update()
 
 
@@ -457,6 +500,10 @@ class GameView:
             return self.gameover_backtomenu_but
 
         else: return self.gameover_pic
+
+    def check_if_best_score(self):
+        if self.score > self.best_score:
+            self.best_score = self.score
 
 
     def restart_game(self):
